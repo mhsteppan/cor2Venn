e68c838b1a99af7bbe6a4e8167450f1e6425483b
Martin Steppan <mhsteppan@gmail.com>
2022-11-10 18:36:55 +0100

    now

---
+++ R/cormat16pf.R
@@ -0,0 +1,25 @@
+#' Correlations of 16 Personality Factors
+#'
+#' Data from items from the International Personality Item Pool
+
+#'
+#' @docType data
+#'
+#' @usage data(cormat16pf)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Possible Questionnaire Format for
+#' (\href{http://openpsychometrics.org/_rawdata/}{Link})
+#'
+#' @source \href{http://openpsychometrics.org/_rawdata/}{}
+#'
+#'
+#' @examples
+#' library(corrplot)
+#' data(cormat16pf)
+#' corrplot(cormat16pf)
+
+"cormat16pf"
