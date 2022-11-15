cc29b50e7f099be91652db36650b9fab9c96993b
Martin Steppan <mhsteppan@gmail.com>
2022-11-15 18:55:18 +0100

    now

---
+++ R/darktriad.R
@@ -0,0 +1,25 @@
+#' Correlations of 16 Personality Factors
+#'
+#' Data from items from the International Personality Item Pool
+
+#'
+#' @docType data
+#'
+#' @usage data(darktriad)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Possible Questionnaire Format for
+#' (\href{http://openpsychometrics.org/_rawdata/}{Link})
+#'
+#' @source \href{http://openpsychometrics.org/_rawdata/}{}
+#'
+#'
+#' @examples
+#' library(corrplot)
+#' data(darktriad)
+#' corrplot(darktriad)
+
+"darktriad"
